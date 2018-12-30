d9bf7b758b785724b57c76594d84db99661a8fff
ctjoy <ctjoychen@gmail.com>
2018-12-30 18:05:22 +0200

    add part2 answer and group information

--- cs236605-hw2/hw2/answers.py
+++ cs236605-hw2/hw2/answers.py
@@ -13,9 +13,9 @@ def part2_overfit_hp():
     wstd, lr, reg = 0, 0, 0
     # TODO: Tweak the hyperparameters until you overfit the small dataset.
     # ====== YOUR CODE: ======
-    wstd = 0.1
-    lr = 0.005
-    reg = 1
+    wstd = 0.01
+    lr = 0.003
+    reg = 0.01
     # ========================
     return dict(wstd=wstd, lr=lr, reg=reg)
 
@@ -27,10 +27,10 @@ def part2_optim_hp():
     # You may want to use different learning rates for each optimizer.
     # ====== YOUR CODE: ======
     wstd = 0.1
-    lr_vanilla = 0.001
-    lr_momentum = 0.001
-    lr_rmsprop = 0.001
-    reg = 0.05
+    lr_vanilla = 0.005
+    lr_momentum = 0.0004
+    lr_rmsprop = 0.0002
+    reg = 0.000005
     # ========================
     return dict(wstd=wstd, lr_vanilla=lr_vanilla, lr_momentum=lr_momentum,
                 lr_rmsprop=lr_rmsprop, reg=reg)
@@ -48,29 +48,22 @@ def part2_dropout_hp():
 
 
 part2_q1 = r"""
-**Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+There appears overfitting when there is no dropout because after 10 epochs
+when training accuracy increases, validation accuracy didn't improve or even decrease.
+After applying dropout=0.4, overfitting is reduced since the test accuracy increases together
+with training accuracy. However, since some random units are not put into the training
+session with dropout, the training speed slows down. With the highest dropout rate=0.8, there
+stills appears overfitting because when training accuracy increases slowly, test accuracy didn't
+improve after 10 epochs.
 """
 
 part2_q2 = r"""
-**Your answer:**
-
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
-
+Yes,it is possible that while test loss to increase for a few epochs 
+while the test accuracy also increases.
+This is easily possible with cross entropy loss function that is sensitive to the
+distance between an incorrect prediction and the ground truth. 
+You can get 90% of the predictions correct, but if the misses are 
+ridiculously far off the mark, your loss value can increase.
 """
 # ==============
 
